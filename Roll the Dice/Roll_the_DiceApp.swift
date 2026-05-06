f97c277e31b965abc2c12123fe78de75d2c51ae2
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-05-06 11:30:44 -0500

    Initial Commit

---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Pamela VanLeirsburg on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
